18e401f79e715b4132b85f064dadd2578df900eb
arnab4477 <arnabsahareal@gmail.com>
2023-03-02 02:04:15 +0530

    Adde some dummy data for some exercises (more to be added later)

---
+++ src/Exercises.js
@@ -0,0 +1,82 @@
+/**
+ * The dummy data for the different movements of different levels
+ */
+const Exercises = [
+  {
+    name: 'Pull Up',
+    description:
+      'Pull up is an essential basic vertical pulling movement, which primarily works the lats. Pull ups help building massive pulling stength which is required for more advanced movements',
+    image:
+      'https://www.muscleandfitness.com/wp-content/uploads/2019/12/Fit-Man-Performing-A-Pullup-In-The-Park.jpg?quality=86&strip=all',
+    tutorials: [
+      'https://www.youtube.com/watch?v=Y3ntNsIS2Q8',
+      'https://www.mpcalisthenics.com/tutorial/pull-up-tutorial',
+    ],
+    skilltype: ['basics', 'strength', 'hypertrophy', 'bent arm strength'],
+    muscles: ['lats', 'biceps', 'forearms'],
+    difficulty: 'Beginner',
+    equipments: ['horizontal bar', 'gymnastics rings'],
+  },
+  {
+    name: 'Push Up',
+    description:
+      'Push up is a basic horizontal pushing movement that primarily works the chest and anterior delts. Push up is one of the most important base building exercises for Calisthenics',
+    image:
+      'https://manofmany.com/wp-content/uploads/2020/02/How-to-do-a-proper-pushup.jpg',
+    tutorials: [
+      'https://www.youtube.com/watch?v=IODxDxX7oi4',
+      'https://barbend.com/push-up-variations/',
+    ],
+    skilltype: ['basics', 'strength', 'hypertrophy', 'bent arm strength'],
+    muscles: ['chest', 'biceps', 'shoulders'],
+    difficulty: 'Beginner',
+    equipments: ['floor', 'gymnastics rings'],
+  },
+  {
+    name: 'Front Lever Raise',
+    description:
+      'Front Lever Raise is one of the best movements to strengthen or work towards the full Front Lever. The athlete starts from a hang and pulls himself to a Front Lever or an inverted hang with straight arms',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvtRwrNZcCLVjR09M2Koe-lxXjuGZJ0sZcnXqDVPjZNBosyJUsmm94_K8LHGu3dqA1FxM&usqp=CAU',
+    tutorials: [
+      'https://www.youtube.com/watch?v=H655GnqpRIM',
+      'https://www.fitstream.com/exercises/front-lever-a6019',
+    ],
+    skilltype: ['strength', 'static', 'straight arm strength'],
+    muscles: ['lats'],
+    difficulty: 'Intermediate',
+    equipments: ['gymnastics rings', 'horizontal bars'],
+  },
+  {
+    name: 'Handstand Push Up',
+    description:
+      'Handstand push up is the main vertical pushing movement in Calisthennics that can be done either freestanding or with wall assistance. HSPU primarily works the shoulders and helps building both strength and muscle mass for the upper body',
+    image:
+      'https://qph.cf2.quoracdn.net/main-qimg-b091b515761da4dabed5bb2756b68b7e-lq',
+    tutorials: [
+      'https://www.youtube.com/watch?v=uZWjFe4fufA',
+      'https://calisthenicsworldwide.com/calisthenics/handstand-push-up/',
+    ],
+    skilltype: ['strength', 'hypertrophy', 'bent arm strength'],
+    muscles: ['triceps', 'shoulders'],
+    difficulty: 'Intermediate',
+    equipments: ['gymnastics rings', 'floor', 'parallel bars'],
+  },
+  {
+    name: 'Iron Cross',
+    description:
+      'Iron Cross is an advanced Gymnastics static skill that requires both immense muscular as well as tendon strength. This is a FIG B rated movement',
+    image:
+      'https://www.researchgate.net/profile/Walter-Frontera/publication/233749968/figure/fig2/AS:667635939282950@1536188220402/A-gymnast-performing-the-Iron-Cross-which-is-executed-by-extending-both-arms-straight_Q640.jpg',
+    tutorials: [
+      'https://www.youtube.com/watch?v=8nbFV-BOEL8',
+      'https://www.fitstream.com/exercises/iron-cross-a50',
+    ],
+    skilltype: ['strength', 'static', 'straight arm strength'],
+    muscles: ['biceps', 'shoulders'],
+    difficulty: 'Advanced',
+    equipments: ['gymnastics rings'],
+  },
+];
+
+export default Exercises;
