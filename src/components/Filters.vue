9036a6b90485d106840e16acefbda36caa2002c5
arnab4477 <arnabsahareal@gmail.com>
2023-03-02 20:05:00 +0530

    Created the filters component with four filters

---
+++ src/components/Filters.vue
@@ -0,0 +1,77 @@
+<script setup>
+import DropDownMenu from './DropDownMenu.vue';
+
+const skills = {
+  type: 'Skills',
+  filterArr: [
+    'Any',
+    'basic',
+    'strength',
+    'hypertrophy',
+    'balance',
+    'bent arm strength',
+    'straight arm strength',
+    'static',
+  ],
+};
+
+const muscles = {
+  type: 'Muscles',
+  filterArr: [
+    'Any',
+    'chest',
+    'lats',
+    'biceps',
+    'triceps',
+    'shoulders',
+    'forearms',
+    'quadriceps',
+    'hamstrings',
+    'glutes',
+  ],
+};
+
+const difficulty = {
+  type: 'Difficulty',
+  filterArr: ['Any', 'Beginner', 'Intermediate', 'Advanced'],
+};
+
+const equipments = {
+  type: 'Equipments',
+  filterArr: [
+    'Any',
+    'floor',
+    'horizontal bar',
+    'gymnastics rings',
+    'parallel bars',
+  ],
+};
+</script>
+
+<template>
+  <div>
+    <DropDownMenu v-bind="skills" />
+    <DropDownMenu v-bind="muscles" />
+    <DropDownMenu v-bind="difficulty" />
+    <DropDownMenu v-bind="equipments" />
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: start;
+
+  width: 100%;
+}
+
+@media (max-width: 550px) {
+  div {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    gap: 12px;
+  }
+}
+</style>
