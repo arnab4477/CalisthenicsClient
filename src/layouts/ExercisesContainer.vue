607de119446389ea4d1094072bf3a833d89507e5
arnab4477 <arnabsahareal@gmail.com>
2023-03-04 16:03:57 +0530

    Created the layout to show the filters and the movements thumbnails together

---
+++ src/layouts/ExercisesContainer.vue
@@ -0,0 +1,72 @@
+<script setup>
+import Filters from '../components/Filters.vue';
+import ExercisesGrid from '../components/ExercisesGrid.vue';
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="heading">Filter through the Movements</h1>
+    <Filters class="filters" />
+    <ExercisesGrid class="exercises" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+
+  padding: 10px;
+
+  background-color: rgba(252, 1, 10, 0.143);
+
+  width: 1000px;
+  height: auto;
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  border-radius: 25px;
+}
+
+.container::-webkit-scrollbar {
+  width: 4px;
+}
+.container::-webkit-scrollbar-thumb {
+  background: rgb(0, 0, 0);
+  border-radius: 100vw;
+}
+.container::-webkit-scrollbar-track {
+  background: rgb(179, 179, 179);
+  border-radius: 100vw;
+}
+
+.heading {
+  position: relative;
+  right: 220px;
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.exercises {
+  position: relative;
+  top: 75px;
+  left: 28px;
+}
+
+@media (max-width: 550px) {
+  .container {
+    width: 375px;
+  }
+
+  .heading {
+    font-size: 28px;
+    right: 0px;
+  }
+
+  .exercises {
+    left: 0px;
+  }
+}
+</style>
