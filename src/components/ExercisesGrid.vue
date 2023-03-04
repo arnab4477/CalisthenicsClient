7b3745d8e25a640c5d771af3d5eb42582a615500
arnab4477 <arnabsahareal@gmail.com>
2023-03-04 16:02:37 +0530

    Created a component to display all the filtered movement thumbnails

---
+++ src/components/ExercisesGrid.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useMovementsStore } from '../store';
+import Thumbnail from './Thumbnail.vue';
+
+const { filteredMovements } = storeToRefs(useMovementsStore());
+</script>
+
+<template>
+  <div class="list-container">
+    <Thumbnail
+      v-for="movement in filteredMovements"
+      :key="movement.name"
+      v-bind="movement"
+    />
+  </div>
+</template>
+
+<style scoped>
+.list-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 16px;
+  padding: 20px;
+}
+@media (max-width: 550px) {
+  .list-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+}
+</style>
