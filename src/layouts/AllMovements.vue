b2fe194fda21aca937444fa8e78a5c464e0e1e11
arnab4477 <arnabsahareal@gmail.com>
2023-03-10 04:59:40 +0530

    Created layouts for showing all and one movement

---
+++ src/layouts/AllMovements.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ExercisesContainer from '../components/ExercisesContainer.vue';
+</script>
+
+<template>
+  <main>
+    <ExercisesContainer />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/layouts/OneMovement.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import Exercises from '../Exercises';
+import ExerciseCard from '../components/ExerciseCard.vue';
+
+const router = useRoute();
+const movementToShow = ref({});
+const { movement } = router.params;
+
+onBeforeMount(() => {
+  movementToShow.value = Exercises.find((m) => m.name === movement);
+});
+</script>
+
+<template>
+  <main>
+    <ExerciseCard v-bind="movementToShow" />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
