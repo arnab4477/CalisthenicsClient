2bbe024b790056353b7ee71149a7ba7cf4240c2e
arnab4477 <arnabsahareal@gmail.com>
2023-03-10 05:00:15 +0530

    Enabled client side routing with vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomeLayout from '../layouts/HomeLayout.vue';
+import AllMovements from '../layouts/AllMovements.vue';
+import OneMovement from '../layouts/OneMovement.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeLayout,
+    },
+    {
+      path: '/movements',
+      name: 'movements',
+      component: AllMovements,
+    },
+    {
+      path: '/movements/:movement',
+      name: 'movement',
+      component: OneMovement,
+    },
+  ],
 });
 
 export default router;
