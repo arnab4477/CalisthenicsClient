d4ded9afc1863e4959f84c839d1fd82bf2594a24
arnab4477 <arnabsahareal@gmail.com>
2023-03-10 04:55:49 +0530

    Created a header component

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/" class="router-link">Home</RouterLink>
+    <RouterLink to="/movements" class="router-link">Movements</RouterLink>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 15px;
+  background-color: darkgreen;
+}
+
+.router-link {
+  text-decoration: none;
+  list-style: none;
+
+  color: white;
+  padding: 10px;
+  font-size: 26px;
+  border-radius: 15px;
+
+  transition: 0.15s;
+}
+
+.router-link:hover {
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.376);
+}
+</style>
